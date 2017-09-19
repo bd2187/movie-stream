3feae55b836eac11fa4a5a39307ac2abd2882ca0
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-09-19 11:53:04 -0700

    Add getRanking function. This function is responsible for sorting the movies array in ascending order by watchCount and returning ranking of movie argument passed into function

--- src/app.js
+++ src/app.js
@@ -10,8 +10,40 @@ function videoViewed(strVideoName) {
   });
 }
 
-// ====== Tests
+function getRanking(strVideoName) {
+  var ranking;
+
+  // sorted movies array in order by watchCount
+
+  const sortedMovieRankings = movies.sort(
+    (next, prev) => prev.watchCount - next.watchCount
+  );
+
+  // For each movie in sortedMovieRankings array,
+  // check if movie.name === strVideoName.
+  // If true, give ranking value of index + 1.
+
+  sortedMovieRankings.forEach(function(movie, index) {
+    if (movie.name === strVideoName) {
+      ranking = index + 1;
+    }
+  });
+
+  return ranking;
+}
+
+// =========== Tests ===========
 videoViewed('The Mummy');
 console.log(`${movies[0].name} views: ${movies[0].watchCount}`);
+
 videoViewed('The Mummy');
 console.log(`${movies[0].name} views: ${movies[0].watchCount}`);
+
+videoViewed('Rough Night');
+console.log(`${movies[6].name} views: ${movies[6].watchCount}`);
+
+videoViewed('Rough Night');
+console.log(`${movies[6].name} views: ${movies[6].watchCount}`);
+
+console.log('Baby Driver ranking:', getRanking('Baby Driver'));
+console.log('All Eyez On Me ranking:', getRanking('All Eyez On Me'));
