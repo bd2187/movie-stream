c3c8264a09239f429f19bd779b481ca076d5e3d1
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-09-19 11:57:00 -0700

    Remove 'rank' property from each object within movies array since watchCount is dynamic and rankings will change

--- data/data.js
+++ data/data.js
@@ -1,53 +1,43 @@
 const movies = [
   {
     name: 'The Mummy',
-    watchCount: 1638096,
-    rank: 1
+    watchCount: 1638096
   },
   {
     name: 'Baby Driver',
-    watchCount: 1476100,
-    rank: 2
+    watchCount: 1476100
   },
   {
     name: 'Captain Underpants: The First Epic Movie',
-    watchCount: 118813,
-    rank: 3
+    watchCount: 1318813
   },
   {
     name: 'It Comes At Night',
-    watchCount: 999547,
-    rank: 4
+    watchCount: 999547
   },
   {
     name: 'Strong Island',
-    watchCount: 998833,
-    rank: 5
+    watchCount: 998833
   },
   {
     name: 'All Eyez On Me',
-    watchCount: 953628,
-    rank: 6
+    watchCount: 953628
   },
   {
     name: 'Rough Night',
-    watchCount: 940397,
-    rank: 7
+    watchCount: 940397
   },
   {
     name: 'Megan Leavey',
-    watchCount: 938431,
-    rank: 8
+    watchCount: 938431
   },
   {
     name: 'Anti Matter',
-    watchCount: 930422,
-    rank: 9
+    watchCount: 930422
   },
   {
     name: 'The Wedding Plan',
-    watchCount: 920538,
-    rank: 10
+    watchCount: 920538
   }
 ];
 
