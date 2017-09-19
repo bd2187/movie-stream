1ff9a173edc40584558a9ad20119c413a5bd550b
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-09-19 11:01:42 -0700

    Remove commas from numbers in data.js due to syntax error

--- data/data.js
+++ data/data.js
@@ -1,52 +1,54 @@
 const movies = [
   {
-    name: "The Mummy",
-    watchCount: 1,638,096,
+    name: 'The Mummy',
+    watchCount: 1638096,
     rank: 1
   },
   {
-    name: "Baby Driver",
-    watchCount: 1,476,100,
+    name: 'Baby Driver',
+    watchCount: 1476100,
     rank: 2
   },
   {
-    name: "Captain Underpants: The First Epic Movie",
-    watchCount: 1,185,813,
+    name: 'Captain Underpants: The First Epic Movie',
+    watchCount: 118813,
     rank: 3
   },
   {
-    name: "It Comes At Night",
-    watchCount: 999,547
+    name: 'It Comes At Night',
+    watchCount: 999547,
     rank: 4
   },
   {
-    name: "Strong Island",
-    watchCount: 998,833,
+    name: 'Strong Island',
+    watchCount: 998833,
     rank: 5
   },
   {
-    name: "All Eyez On Me",
-    watchCount: 953,628,
+    name: 'All Eyez On Me',
+    watchCount: 953628,
     rank: 6
   },
   {
-    name: "Rough Night",
-    watchCount: 940,397,
+    name: 'Rough Night',
+    watchCount: 940397,
     rank: 7
   },
   {
-    name: "Megan Leavey",
-    watchCount: 938,431,
+    name: 'Megan Leavey',
+    watchCount: 938431,
     rank: 8
   },
   {
-    name: "Anti Matter",
-    watchCount: 930,422,
+    name: 'Anti Matter',
+    watchCount: 930422,
     rank: 9
   },
   {
-    name: "The Wedding Plan",
-    watchCount: 920,538,
+    name: 'The Wedding Plan',
+    watchCount: 920538,
     rank: 10
   }
-]
+];
+
+export default movies;
