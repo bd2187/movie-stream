ece7e96675b764631f1b3b569da362635305dcc8
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-09-19 11:18:43 -0700

    Implement videoViewed function to be called whenever video is viewed. The function should increment a video's view count by one whenever called. Also, display tests

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 import movies from '../data/data';
+
+function videoViewed(strVideoName) {
+  // For each movie in movies array,
+  // check if movie.name matches strVideoName.
+  // If true, increment movie's watch count by 1
+
+  movies.forEach(function(movie) {
+    return movie.name === strVideoName ? movie.watchCount++ : null;
+  });
+}
+
+// ====== Tests
+videoViewed('The Mummy');
+console.log(`${movies[0].name} views: ${movies[0].watchCount}`);
+videoViewed('The Mummy');
+console.log(`${movies[0].name} views: ${movies[0].watchCount}`);
